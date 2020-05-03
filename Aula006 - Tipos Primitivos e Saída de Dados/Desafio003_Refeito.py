27a0ca4b52461df5b5285d02e16b75169193a0db
antunesce <antunes.diego@gmail.com>
2020-05-03 13:48:36 -0300

    Atualizando a aula 6 e desafio 3 e 4

---
+++ Aula006 - Tipos Primitivos e Saída de Dados/Desafio003_Refeito.py
@@ -0,0 +1,9 @@
+# Desafio 003: Crie um programa que leia dois números e mostre a soma entre eles.
+
+print("Vamos brincar de somar!")
+n1 = int(input("Digite um número qualquer: "))
+n2 = int(input("Digite outro número qualquer: "))
+
+s = n1 + n2
+
+print("{} + {} = {}".format(n1, n2, s))
--- Aula006 - Tipos Primitivos e Saída de Dados/Desafio004.py
+++ Aula006 - Tipos Primitivos e Saída de Dados/Desafio004.py
@@ -1,1 +1,13 @@
-# Desafio 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis.
+# Desafio 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+v = input("Digite algo: ")
+
+print('Você digitou "{}" e o tipo primitivo que ele percente é {}.'.format(v, type(v)))
+print('"{}" é um número? {}.'.format(v, v.isnumeric()))
+print('"{}" é um número decimal? {}.'.format(v, v.isdecimal()))
+print('"{}" é um número alfanumérico? {}'.format(v, v.isalnum()))
+print('"{}" só tem letras do alfabético? {}'.format(v, v.isalpha()))
+print('"{}" só tem espaço? {}.'.format(v, v.isspace()))
+print('"{}" tem o primeiro caractere em letra maiúscula? {}.'.format(v, v.istitle()))
+print('"{}" está todo em minúscula? {}.'.format(v, v.islower()))
+print('"{}" está todo em maiúscula? {}.'.format(v, v.isupper()))
