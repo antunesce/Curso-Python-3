96e9e83e7c0958e3ff6d8fedf92ccda99d4977b1
antunesce <antunes.diego@gmail.com>
2020-05-05 18:28:43 -0300

    Atualização da aula 008

---
+++ Aula008 - Utilizando Módulos/Desafio017.py
@@ -0,0 +1,1 @@
+# Desafio 017: Faça um programa que leia o comprimento do cateto oposto e do cateto adjascente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
---
+++ Aula008 - Utilizando Módulos/Desafio019.py
@@ -0,0 +1,1 @@
+# Desafio 019: Um professor quer sortear um dos seus quatros alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.
