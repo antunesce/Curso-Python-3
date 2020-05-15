8ffc0de0accf872d3ffad301db219ddc7c595deb
antunesce <antunes.diego@gmail.com>
2020-05-14 22:21:25 -0300

    Resolução dos Desafios 28 e 29

--- Aula010 - Condições (Parte 1)/Desafio028.py
+++ Aula010 - Condições (Parte 1)/Desafio028.py
@@ -1,2 +1,21 @@
 # Desafio 028: Escreva um programa que faço o computador pensar em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o númro escolhido pelo computador. 
-# O programa deverá escrever na tela se o usuário venceu ou perdeu. 
+# O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+from random import randint
+print('-' * 60)
+print('{:^60}'.format('Jogo do Adivinha qual o número'))
+print('-' * 60)
+numero = int(input('Vamos brincar de adivinhar, escolha um número de 0 a 5: '))
+
+numero_aleatorio = randint(0, 5)
+
+print('-' * 60)
+print('')
+if numero == numero_aleatorio:
+    print('Parabéns, você acertou! :D')
+    print(f'Você escolheu {numero} e o computador {numero_aleatorio}.')
+else:
+    print('Pennnn, você errou! :p')
+    print(f'Você escolheu {numero} e o computador {numero_aleatorio}.')
+print('')
+print('-' * 60)
--- Aula010 - Condições (Parte 1)/Desafio029.py
+++ Aula010 - Condições (Parte 1)/Desafio029.py
@@ -1,1 +1,17 @@
 # Desafio 029: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$ 7,00 por km acima do limite.
+
+print('-' * 60)
+print('{:^60}'.format('Medidor de velocidade'))
+print('-' * 60)
+velocidade = float(input('Digite a velocidade do automóvel (km): '))
+print('-' * 60)
+print('')
+
+if velocidade > 80:
+    acima_do_limite = velocidade - 80
+    multa = acima_do_limite * 7
+    print(f'Você ultrapassou {acima_do_limite:0.1f} km/h, a multa a ser paga é de R$ {multa:0.1f}.')
+else:
+    print('O motorista não ultrapasou o limite de 80 km/h, Parabéns!')
+print('')
+print('-' * 60)
