37b76bf8554e5203def37032041d0343a1bcd2cf
antunesce <antunes.diego@gmail.com>
2020-05-15 20:05:32 -0300

    Resolução dos Desafios 30 e 31

--- Aula010 - Condições (Parte 1)/Desafio030.py
+++ Aula010 - Condições (Parte 1)/Desafio030.py
@@ -1,1 +1,16 @@
 # Desafio 030: Crie um programa que leia um número inteiro qualquer e mostre na tela se ele é PAR ou ÍMPAR.
+
+print('-' * 60)
+print('{:^60}'.format('Par ou Ímpar'))
+print('-' * 60)
+print('')
+numero = int(input('Digite um número: '))
+print('')
+
+
+print('')
+if (numero % 2) == 0:
+    print(f'Você digitou o número {numero} e é PAR.')
+else:
+    print(f'Você digitou o número {numero} e é ÍMPAR.')
+print('-' * 60)
--- Aula010 - Condições (Parte 1)/Desafio031.py
+++ Aula010 - Condições (Parte 1)/Desafio031.py
@@ -1,1 +1,17 @@
 # Desafio 031: Desenvolva um programa que pergunte a distância de uma viagem em km. Calcule o preço da passagem, cobrando R$ 0,50 por km para viagens até 200 km e R$ 0,45 para viagens mais longas.
+
+print('-' * 60)
+print('{:^60}'.format('Calculadora de quilometragem'))
+print('-' * 60)
+print('')
+km = float(input('Digite a quilometragem percorrida: '))
+print('')
+print('-' * 60)
+
+if km <= 200:
+    valor = km * 0.5
+    print(f'Você percorreu {km:0.1f} km, o valor a ser pago é R$ {valor:0.2f}.')
+else:
+    valor = km * 0.45
+    print(f'Você percorreu {km:0.1f} km, o valor a ser pago é R$ {valor:0.2f}.')
+print('-' * 60)
