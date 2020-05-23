e002c512384a445a90af7a0e250460fd7f532101
Diego Antunes <antunes.diego@gmail.com>
2020-05-23 17:35:16 -0300

    Desafios 41

--- Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio041.py
+++ Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio041.py
@@ -6,3 +6,28 @@
 - Até 20 anos: Sênior
 - Acima: Master
 '''
+
+print('-' * 60)
+print('{:^60}'.format('Classificador categórico'))
+print('-' * 60)
+ano_nascimento = int(input('Digite a sua data de nascimento (19XX): '))
+print('-' * 60)
+print('')
+
+from datetime import date
+
+ano = date.today().year
+idade = ano - ano_nascimento
+
+if idade < 10:
+    print(f' Você está na catergoria MIRIM.')
+elif idade > 9 and idade < 15:
+    print(f'Você está na categoria INFANTIL.')
+elif idade > 14 and idade < 20:
+    print('Você está na catergoria JUNIOR.')
+elif idade == 20:
+    print('Você está na categoria SÊNIOR.')
+else:
+    print('Você está na categoria MASTER.')
+print('')
+print('-' * 60)
