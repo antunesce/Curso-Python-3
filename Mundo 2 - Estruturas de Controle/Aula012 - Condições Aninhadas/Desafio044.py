d1079a0b6e7e1736a29a68f00349da06686f3164
Diego Antunes <antunes.diego@gmail.com>
2020-05-24 14:10:44 -0300

    Desafio 044

--- Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio044.py
+++ Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio044.py
@@ -11,3 +11,60 @@ print('{:^60}'.format('Calculadora de pagamento v1.0'))
 print('-' * 60)
 valor = float(input('Qual o valor do produto R$ '))
 print('-' * 60)
+print('')
+
+print('=' * 60)
+print('{:^60}'.format('Forma de pagamento'))
+print('=' * 60)
+print('1 - à vista dinheiro/cheque')
+print('2 - à vista no cartão')
+print('3 - parcelado')
+print('')
+opcao = int(input('Digite a opção: '))
+print('=' * 60)
+
+if opcao == 1:
+    print('{:^60}'.format('Opção selecionada: "à vista dinheiro/cheque"'))
+    print('=' * 60)
+    print('')
+    desconto = valor * 10 / 100
+    valor_final = valor - desconto
+    print(f'Valor bruto:                (=) R$ {valor:0.2f}')
+    print(f'Valor do desconto (10%):    (-) R$ {desconto:0.2f}')
+    print('')
+    print(f'Valor total a pagar:        (=) R$ {valor_final:0.2f}')
+elif opcao == 2:
+    print('{:^60}'.format('Opção selecionada: "à vista no cartão"'))
+    print('=' * 60)
+    print('')
+    desconto = valor * 5 / 100
+    valor_final = valor - desconto
+    print(f'Valor bruto:                (=) R$ {valor:0.2f}')
+    print(f'Valor do desconto (5%):     (-) R$ {desconto:0.2f}')
+    print('')
+    print(f'Valor total a pagar:        (=) R$ {valor:0.2f}.')
+elif opcao == 3:
+    print('{:^60}'.format('Opção selecionada: "parcelado"'))
+    print('=' * 60)
+    parcelas = int(input('Quantidade de parcelas: '))
+    print('=' * 60)
+    print('')
+    if parcelas <= 2:
+        valor_das_parcelas = valor / parcelas
+        print(f'Valor bruto:            (=) R$ {valor:0.2f}')
+        print(f'{parcelas} parcela(s)            (x) R$ {valor_das_parcelas:0.2f}')
+        print('')
+        print(f'Valor total a prazo:    (=) R$ {valor:0.2f}')
+    else:
+        juros = valor * 20 /100
+        valor_final = valor + juros
+        valor_das_parcelas = valor_final / parcelas
+        print(f'Valor bruto:            (=) R$ {valor:0.2f}')
+        print(f'{parcelas:>2} Parcela(s)           (x) R${valor_das_parcelas: 0.2f}')
+        print(f'')
+        print(f'Valor total a prazo     (=)R$ {valor_final:0.2f}')
+else:
+    print('')
+    print('\033[1;31mOpção inválida!\033[m')
+print('')
+print('-' * 60)
--- Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio045.py
+++ Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio045.py
@@ -1,1 +1,5 @@
 # Desafio 045: Crie um programa que faça o computador jogar JOKENPÔ com você.
+
+print('=' * 60)
+print('{:^60}'.format('Jokenpô v1.0'))
+print('=' * 60)
