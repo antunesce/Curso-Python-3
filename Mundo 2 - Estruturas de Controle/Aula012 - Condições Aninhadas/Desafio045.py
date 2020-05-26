6a8eeccb5be43ce6236a59db049d638ed8c8ff41
Diego Antunes <antunes.diego@gmail.com>
2020-05-25 23:25:55 -0300

    Desafio 045

--- Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio045.py
+++ Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio045.py
@@ -1,22 +1,39 @@
 # Desafio 045: Crie um programa que faça o computador jogar JOKENPÔ com você.
 
+import emoji
+from random import randint
+
+# Título do script
 print('=' * 60)
 print('{:^60}'.format('Jokenpô v1.0'))
 print('=' * 60)
 print('')
+
+# Instruções do game
 print('''Instruções:
 \033[1;31m1\033[m - \033[1;31mPedra\033[m empata com pedra e ganha de Tesoura;
 \033[1;32m2\033[m - \033[1;32mTesoura\033[m empata com Tesoura e ganha de Papel;
 \033[1;33m3\033[m - \033[1;33mPapel\033[m empata com Papel e ganha de Pedra.''')
 print('')
+
+# Opção do player
 p1 = int(input('Escolha uma das três opções: '))
 print('')
 print('=' * 60)
+print('')
+
+# Opção do NPC
+npc = randint(1, 3)
+
+# Teste do game
 
 
 
 
 '''
+print(emoji.emojize(":earth_americas:", use_aliases=True))
+    use_aliases=TRUE permite que o desenho do emoji aparece, senão for digitado aparecerá só o código.
+
 Papel = :raised_hand_with_fingers_splayed:
 pedra = :fist_right: or :fist_left:
 tesoura = :v:
