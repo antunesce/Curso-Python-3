a5dd4f52ad82caabcbcec85fbcee6ebcba5e7a25
Diego Antunes <antunes.diego@gmail.com>
2020-05-26 12:51:33 -0300

    Resolvido Desafio 045

--- Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio045.py
+++ Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio045.py
@@ -11,12 +11,13 @@ print('')
 
 # Instruções do game
 print('''Instruções:
-\033[1;31m1\033[m - \033[1;31mPedra\033[m empata com pedra e ganha de Tesoura;
-\033[1;32m2\033[m - \033[1;32mTesoura\033[m empata com Tesoura e ganha de Papel;
-\033[1;33m3\033[m - \033[1;33mPapel\033[m empata com Papel e ganha de Pedra.''')
+\033[1;31m1\033[m - \033[1;31mPedra\033[m empata com Pedra e ganha de Tesoura;
+\033[1;32m2\033[m - \033[1;32mPapel\033[m empata com Papel e ganha de Pedra;
+\033[1;33m3\033[m - \033[1;33mTesoura\033[m empata com Tesoura e ganha de Papel.''')
 print('')
 
 # Opção do player
+name = str(input('Qual o seu nome: ')).strip().title()
 p1 = int(input('Escolha uma das três opções: '))
 print('')
 print('=' * 60)
@@ -26,18 +27,46 @@ print('')
 npc = randint(1, 3)
 
 # Teste do game
+# Hipótese de vitória
+if p1 == 1 and npc == 3:
+    print(f'{name} (\033[1;31mPedra\033[m) x (\033[1;33mTesoura\033[m) NPC')
+    print('')
+    print('Você \033[1;32mVENCEU!!!\033[m')
+elif p1 == 2 and npc == 1:
+    print(f'{name} (\033[1;32mPapel\033m) x (\033[1;31mPedra\033[m) NPC')
+    print('')
+    print('Você \033[1;32mVenceu!!!\033[m')
+elif p1 == 3 and npc == 2:
+    print(f'{name} (\033[1;33mTesoura\033[m) x (\033[1;32mPapel\033[m) NPC')
+    print('')
+    print('Você \033[1;32mVenceu!!!\033[m')
+# Hipotese de derrota
+elif p1 == 1 and npc == 2:
+    print(f'{name} (\033[1;31mPedra\033[m) x (\033[1;32mPapel\033[m) NPC')
+    print('')
+    print('Você \033[1;31mPerdeu!!!\033[m')
+elif p1 == 2 and npc == 3:
+    print(f'{name} (\033[1;32mPapel\033[m) x (\033[1;33mTesoura\033[m) NPC')
+    print('')
+    print('Você \033[1;31mPerdeu!!!\033[m')
+elif p1 == 3 and npc == 1:
+    print(f'{name} (\033[1;33mTesoura\033[m) x (\033[1;31mPedra\033[m) NPC')
+    print('')
+    print('Você \033[1;31mPerdeu!!!\033[m')
+# Hipótese de empate
+else:
+    if p1 and npc == 1:
+        p1 = '\033[1;31mPedra\033[m'
+        npc = '\033[1;31mPedra\033[m'
+    elif p1 and npc == 2:
+        p1 = '\033[1;32mPapel\033[m'
+        npc = '\033[1;32mPapel\033[m'
+    else:
+        p1 = '\033[1;33mTesoura\033[m'
+        npc = '\033[1;33mTesoura\033[m'
+    print(f'{name} ({p1}) x ({npc}) NPC')
+    print('')
+    print('Houve \033[1;33mEmpate!!!\033[m')
 
-
-
-
-'''
-print(emoji.emojize(":earth_americas:", use_aliases=True))
-    use_aliases=TRUE permite que o desenho do emoji aparece, senão for digitado aparecerá só o código.
-
-Papel = :raised_hand_with_fingers_splayed:
-pedra = :fist_right: or :fist_left:
-tesoura = :v:
-
-win = :smile: :trophy:
-loser = :sob:
-'''
+print('')
+print('*' * 60)
