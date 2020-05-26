21056a820e7fe58e6536339c76ebe41145e3ec97
Diego Antunes <antunes.diego@gmail.com>
2020-05-26 13:20:32 -0300

    Resolução Desafio 037

--- Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio037.py
+++ Mundo 2 - Estruturas de Controle/Aula012 - Condições Aninhadas/Desafio037.py
@@ -4,3 +4,34 @@
 2 - para octal
 3 - para hexadecimal
 '''
+
+print('=' * 60)
+print('{:^60}'.format('Conversor numérico v1.0'))
+print('=' * 60)
+number = int(input('Digite um valor a ser convertido: '))
+print('-' * 60)
+print('''Opções para conversão:
+1 - Binário
+2 - Octal
+3 - Hexadecimal''')
+option = int(input('Digite a opção escolhida: '))
+print('=' * 60)
+print('')
+
+if option == 1:
+    number_binary = bin(number)
+    print(f'de Decimal: {number}')
+    print(f'para Binário: {number_binary}')
+elif option == 2:
+    number_octal = oct(number)
+    print(f'de Decimal: {number}')
+    print(f'para Octal: {number_octal}')
+elif option == 3:
+    number_hexdecimal = hex(number)
+    print(f'de Decimal: {number}')
+    print(f'para Hexadecimal: {number_hexdecimal}')
+else:
+    print('Opção inválida!')
+
+print('')
+print('=' * 60)
